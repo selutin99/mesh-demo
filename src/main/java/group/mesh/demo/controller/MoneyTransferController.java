1b6b6d1c8cac71462e024f1c313ba49658847ebb
Селютин Александр Дмитриевич <cool.selutin99@yandex.ru>
2022-07-07 23:33:17 +0400

    Add money transfer

--- src/main/java/group/mesh/demo/controller/MoneyTransferController.java
+++ src/main/java/group/mesh/demo/controller/MoneyTransferController.java
@@ -2,6 +2,9 @@ package group.mesh.demo.controller;
 
 import group.mesh.demo.domain.dto.request.MoneyTransferRequestDto;
 import group.mesh.demo.service.MoneyTransferService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("transfer")
 @RequiredArgsConstructor
+@Api(tags = "Money Transfer")
 public class MoneyTransferController {
 
     private final MoneyTransferService moneyTransferService;
 
     @PostMapping
+    @ApiOperation(value = "Transfer money", authorizations = {@Authorization(value = "jwtToken")})
     public ResponseEntity<Object> transferMoney(@RequestBody MoneyTransferRequestDto moneyTransferRequest) {
         moneyTransferService.transfer(moneyTransferRequest.getTo(), moneyTransferRequest.getValue());
         return ResponseEntity.status(HttpStatus.OK).body("OK");
